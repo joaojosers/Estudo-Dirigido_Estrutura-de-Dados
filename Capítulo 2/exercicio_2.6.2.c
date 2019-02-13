ce6a82b5d5ea454720e36edb0ec77ba908a344e7
joaojosers <joaojoserodeso@gmail.com>
2019-02-13 21:45:38 -0200

    exercicios do capítulo 2

---
+++ Capítulo 2/exercicio_2.6.2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int num;
+    scanf("%d\n", &num);
+    printf("%d\n, num);
+    system("pause");
+
+    return 0;
+}
---
+++ Capítulo 2/exercicio_2.6.3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int num;
+    scanf("%d", &num);
+    printf("Valor lido: %d\n", num);
+    //system("pause");
+
+    return 0;
+}
