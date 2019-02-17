cc42e00315f2ee63675b872b7e9f3b3b6d55edec
joaojosers <joaojoserodeso@gmail.com>
2019-02-17 17:28:34 -0300

    Exercicio do capitulo 2

--- Capítulo 2/exercicio_2.6.5.c
+++ Capítulo 2/exercicio_2.6.5.c
@@ -6,6 +6,9 @@ int main(){
     scanf("%f", &num);
     printf("Valor lido: %d\n", num);
     
-    /* Na execucao do programa, apareceu a advertencia de que o formato %d pede um argumento do tipo inteiro mas o argumento tem formato de double. E na compilacao, o valor lido foi 1, diferente do valor digitado 3.25. */ 
+    /* Na execucao do programa, apareceu a advertencia
+    de que o formato %d pede um argumento do tipo inteiro
+    mas o argumento tem formato de double. E na compilacao,
+    o valor lido foi 1, diferente do valor digitado 3.25.*/ 
     return 0;
 }
