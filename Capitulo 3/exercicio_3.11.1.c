94a1e7b62fad112db81224e8e243b50d672af1cd
joaojosers <joaojoserodeso@gmail.com>
2019-02-20 22:37:11 -0300

    Add files via upload

---
+++ Capitulo 3/exercicio_3.11.1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    
+    int num;
+    scanf("%d", &num);
+     num--;
+     printf("antecessor = %d\n",num);
+     num++;
+     num++;
+     printf("sucessor = %d\n", num);
+     
+    return 0;
+}
