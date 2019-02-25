6d49560ca16f8c594c0f7437e2b14c48adae1f2a
joaojosers <joaojoserodeso@gmail.com>
2019-02-25 10:13:27 -0300

    Exercício Capítulo 3

---
+++ Capitulo 3/exercicio_3.11.14.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <math.h>
+
+int main(){
+    
+    char caractere;
+    caractere = getchar();
+
+    
+    
+    printf("Codigo ASCii: %d\n", caractere+32);
+    printf("%c\n",caractere+32);
+     
+    return 0;
+}
