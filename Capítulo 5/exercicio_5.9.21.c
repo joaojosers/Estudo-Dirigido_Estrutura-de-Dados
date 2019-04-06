c32a1ec158195744191a15579ffc254ddd2c8a2f
joaojosers <joaojoserodeso@gmail.com>
2019-04-06 20:35:02 -0300

    Exercícios

---
+++ Capítulo 5/exercicio_5.9.21.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main() {
+
+    int aux, n=0, num, i, j, qtd=0,maior=-1000000;
+
+    printf("digite um numero inteiro positivo\n");
+    do{
+        scanf("%d",&num);
+    }while (num<=0);
+    printf("digite %d numeros inteiros\n", num);
+    
+    
+    for (i=1; i<=num; i++){
+        scanf("%d", &n);
+    
+        if(n>maior){
+            maior=n;
+            qtd=1;
+        } else if (maior==n){
+            qtd++;
+        }  
+              
+    }
+        
+   
+    
+    printf("maior = %d\n", maior);
+    printf("numero de vezes que o maior numero foi lido = %d\n", qtd);
+
+    return 0;
+    
+}
