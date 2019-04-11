f8a708a064a7af44c336051a987327ba3590ce4a
joaojosers <joaojoserodeso@gmail.com>
2019-04-11 18:31:38 -0300

    exercícios

---
+++ Capîtulo 6/exercicio_6.7.1.1.c
@@ -0,0 +1,20 @@
+# include <stdio.h>
+# include <stdlib.h>
+#include <math.h>
+
+int main () {
+
+    int i, vet[6];
+    for (i=0; i<6; i++) {
+        scanf("%d", &vet[i]);
+        printf("%d\n", vet[i]);
+    }
+
+
+
+
+
+
+
+    return 0;
+}
